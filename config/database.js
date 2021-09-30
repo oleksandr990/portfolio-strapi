91ba6df280a2d3886ac9034639dea41d4d0eaa3c
arti-lab <artimurataj45@gmail.com>
2021-09-30 03:25:02 +0100

    update strapi version to 3.6.8

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ module.exports = ({ env }) => {
           database,
           username: user,
           password,
-          ssl: { rejectUnauthorized: false },
+          ssl: env.bool("DATABASE_SSL", false),
         },
         options: {},
       },
