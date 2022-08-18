806886947d89f7b5a57fbdfb02216189a5aa0ef9
arti-lab <artimurataj45@gmail.com>
2022-08-18 21:12:15 +0100

    set api Token salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'oIpibAUoGPzZ5bGR')
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6b0d415b946ee440dd297656a12e310f'),
   },
